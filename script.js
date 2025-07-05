e2d4f68e8654740ec1f00e4b41c90a5548025504
nopalarip <96343090+gifts-for-u@users.noreply.github.com>
2025-07-05 09:02:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function showMessage1() {
+  alert(
+    "Sayangku cintaku cantik akuuu pertama mamas mau minta maaaff karena semalem kita berantem dan nda ngasi yang kamu butuhin banget, yaitu emotional connection dari aku, mamas minta maaff nda ngurangin capeknya kamuuu🥺❣️"
+  );
+}
+
+function showMessage2() {
+  alert(
+    "Mamas bukan pasangan yang baik ya? gabisa kasi kenyamanan buat kamu, gabisa kasi emotional connection yang kamu butuhin banget, mamas bikin kamu tambah capek dan marah-marah sampe teriak-teriak, mamas bukan pasangan yang baik yaa..😔 mamas nyesel udah ngebiarin kamu capek dan ngebiarin kamu marah-marah semalem. Harusnya kan kamu mamas manja-manja, mamas tunjukkin kasih sayang mamas, jangan ngebiarin kamu bobonya malem-malem, tapi mamas ga lakuin itu semua.. mamas bukan laki-laki yang baik ya sayang ya?"
+  );
+}
+function showMessage3() {
+  alert(
+    "Kalo kamu masi mau marah, ndapapa mamas paham, mamas akan dengerin kamu, mamas akan nenangin kamu, mamas akan kasih kamu waktu buat marah-marah, dan mamas nantinya akan nenangin kamu, ngasih kamu emotional connection yang lagi kamu butuhin banget, mamas akan bikin kamu ngerasa nyaman dan tenang, mamas akan ngasi waktu kamu buat cerita semua hal yang kamu pendem dari kemarin-kemarin, mamas akan dengerin semua cerita kamu sampe kamu lega karena udah bisa nyeritain semuanya, mamas sayang dede, mamas cinta dede, mamas sayang dede banya banyaaa, mamas cinta dede seniun niunnn lope lopee sekebonnn❣️❣️"
+  );
+}
+
+function showMessage4() {
+  alert(
+    "Padahal kan semalem harusnya kamu bisa manja-manja sama aku, bisa cerita-cerita banyak hal yang bikin kamu capek dari kemarin, bisa gibah juga mungkin nanti ujungnyaa, dan kemungkinan besar juga ketawa-ketawa bareng, tapi mamas malah bikin kamu makin capek dan marah-marah, maafin mamas ya sayangg🥺❣️"
+  );
+}
+function showMessage5() {
+  alert(
+    "Sayangku cintakuu, kamu hebat bisa ngelewatin beberapa hari kemarin yang itu cuapekk buangett, sampe sampe kamu stress banget harus nyari tutor pengganti dan itu ga mudah, dan sampe nanti ada tutor penggantinya, kelas yang kamu pegang akan bertambah karena gaada yang handle. Mamas yakin itu akan sangat menguras tenaga, pikiran, dan emosi kamuu, tapi mamas yakin kamu bisa ngelewatin itu semua sayangg, karena mamas tau kamu kuat, kamu hebat, walopun emang capek banget tapi mamas yakin kamu bisa sayangg🥺❣️"
+  );
+}
+function showMessage6() {
+  alert(
+    "Dedeee.. mamas mau bilang terima kasih sama dede udah bertahan sejauh ini, selama ini sama mamass, terima kasih dede udah sabar dan tahan dari semua yang ada. Dede hebat banget udah sesabar itu dan sekuat itu untuk bertahan sama aku, terus kaya gitu ya sayangg, mamas mau kamu jangan kemana-manaa.. sama mamas terus yaa dede sayanggg, love u to the moon and back, in every universe, in every timeline, in every dimension, in every reality, in every world, in every life, in every existence, in every space and time, in every moment. I love you 3 Million sayanggg❣️"
+  );
+}
